4f5b53d7d3650d59b2a4688cd00e505539c959c3
sjfgzzwd9w-maker <sjfgzzwd9w@privaterelay.appleid.com>
2026-07-23 23:52:12 -0700

    Add ambient drifting background

---
+++ src/components/AmbientBackground.tsx
@@ -0,0 +1,8 @@
+export function AmbientBackground() {
+  return (
+    <div className="ambient-background" aria-hidden="true">
+      <div className="blob blob-blue" />
+      <div className="blob blob-pink" />
+    </div>
+  );
+}
